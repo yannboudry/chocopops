9ef8fa8d8b2057bad32977e3bef88e6affc86536
Yann Boudry <yann.boudry@epita.fr>
2023-09-27 19:39:10 +0200

    FIXED collision with left wall

--- game/shoot.js
+++ game/shoot.js
@@ -61,6 +61,8 @@ function player_collision()
     var x = player1.graphic.position.x + WIDTH / 2;
     var y = player1.graphic.position.y + HEIGHT / 2;
 
+    if (x < 0)
+        player1.graphic.position.x -= x;
     if ( x > WIDTH )
         player1.graphic.position.x -= x - WIDTH;
     if ( y < 0 )
